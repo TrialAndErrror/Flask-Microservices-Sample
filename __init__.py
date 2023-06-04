4eb8be5e5be46a2bbee196448feb4bcf823f70f3
Trial And Errror <coding@trialanderrror.com>
2023-06-04 18:01:46 -0400

    updates after deployment

---
+++ __init__.py
@@ -0,0 +1,33 @@
+from logging.config import dictConfig
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+HANDLER_ENDPOINT = os.getenv("HANDLER_URL", f'http://handler:8000/api')
+
+
+def frontend_app_factory():
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+
+    app = Flask(__name__, template_folder="templates", static_folder="static")
+
+    return app
+
+
+app = frontend_app_factory()
